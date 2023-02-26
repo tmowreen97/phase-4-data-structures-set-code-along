76379620f51eeb41383123cb9978967d17981cf7
Tasreen Mowreen <tmowreen97@gmail.com>
2023-02-25 21:19:28 -0500

    without bonus

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,43 @@
 # your code here
+require 'pry'
+class MySet
+  attr_accessor :value
+  def initialize(enumerable = [])
+    @hash = {}
+    @array =[]
+    enumerable.each do |value|
+      @hash[value] = true
+    end
+  end
+
+  def include?(value)
+    @hash.has_key?(value)
+  end
+
+  def add(value)
+    @hash[value] = true
+    self
+  end
+
+  def delete(value)
+    @hash.delete(value)
+    self
+  end
+
+  def size
+    @hash.size
+  end
+
+  # def self.[](*value)
+  #   # binding.pry
+  #   value.each do |value|
+  #     @hash[value]= true
+  #   end
+  #   binding.pry
+  #   self
+    
+  # end
+
+end
+
+# MySet.delete(8) $
